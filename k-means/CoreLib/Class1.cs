884ee088b7fa6c1d893a8f8d01e9d061f0ca23e9
Vladyslav Polishchuk <vpolishchuk@amcbridge.com>
2017-02-17 21:53:07 +0200

    Add Evklid's methric

--- k-means/CoreLib/Class1.cs
+++ k-means/CoreLib/Class1.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace CoreLib
 {
-    public class Class1
+    internal static class Metric
     {
+        public static double Evklid(List<double> x, List<double>y)
+        {
+            if (x.Count != y.Count)
+                throw new InvalidOperationException("Length of x array is not equel to length of y array");
+            double res = 0;
+            for (int i = 0; i < x.Count; i++)
+            {
+                res += Math.Pow(x[i] - y[i], 2);
+            }
+            return Math.Sqrt(res);
+        }
     }
 }
